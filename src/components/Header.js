7c3371338b4aeea4c0a91dede07768d93b34b8cf
渋谷圭太 <keitax105@gmail.com>
2025-09-19 09:32:05 +0900

    自分がコーディングしたファイルのインデントを半角スペース2つで統一。 プラグインをインストール。tailwind.config.js に追加。表示箇所にline-clamp-2クラスを付与（index.js）

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,14 @@
 import React from "react";
 
 export default function Header() {
-    return (
-        <header className="items-center bg-[#333] text-white flex font-bold justify-between p-6">
-            <div className="container mx-auto flex items-center justify-between">
-                <div className="flex gap-6 justify-between w-full">
-                    <a href="/" className="text-white no-underline">Blog</a>
-                    <a href="/" className="text-white no-underline">お問い合わせ</a>
-                </div>
-            </div>
-        </header>
-    )
+  return (
+    <header className="items-center bg-[#333] text-white flex font-bold justify-between p-6">
+      <div className="container mx-auto flex items-center justify-between">
+        <div className="flex gap-6 justify-between w-full">
+          <a href="/" className="text-white no-underline">Blog</a>
+          <a href="/" className="text-white no-underline">お問い合わせ</a>
+        </div>
+      </div>
+    </header>
+  )
 }
--- src/index.js
+++ src/index.js
@@ -5,38 +5,41 @@ import Header from './components/Header';
 import { posts } from './data/posts';
 
 function Posts() {
-    return (
-        <>
-            {posts.map((p) => (
-                <article key={p.id} className='border border-gray-300 p-4 mb-8 shadow-sm'>
-                    <h2 className='text-2xl font-bold mb-2'>{p.title}</h2>
-                    <img src={p.thumbnailUrl} alt={p.title} className='w-full h-auto mb-2' />
-                    <div className='text-sm text-gray-500 mb-2'>
-                        {new Date(p.createdAt).toLocaleString()}
-                    </div>
-                    <div className='mb-2'>
-                        {p.categories.map((c) => (
-                            <span key={c} className='text-xs bg-gray-200 rounded px-2 py-2 mr-2'>{c}</span>
-                        ))}
-                    </div>
-                    <div dangerouslySetInnerHTML={{ __html: p.content }} />
-                </article>
+  return (
+    <>
+      {posts.map((p) => (
+        <article key={p.id} className='border border-gray-300 p-4 mb-8 shadow-sm'>
+          <h2 className='text-2xl font-bold mb-2'>{p.title}</h2>
+          <img src={p.thumbnailUrl} alt={p.title} className='w-full h-auto mb-2' />
+          <div className='text-sm text-gray-500 mb-2'>
+            {new Date(p.createdAt).toLocaleString()}
+          </div>
+          <div className='mb-2'>
+            {p.categories.map((c) => (
+              <span key={c} className='text-xs bg-gray-200 rounded px-2 py-2 mr-2'>{c}</span>
             ))}
-        </>
-    )
+          </div>
+          <div
+            className='line-clamp-2 overflow-hidden'
+            dangerouslySetInnerHTML={{ __html: p.content }}
+          />
+        </article>
+      ))}
+    </>
+  )
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 // const dest = 'https://ja.react.dev';
 root.render(
-    <>
-        <Header />
-        <main>
-            <div className="container mx-auto px-4">
-                <div className='max-w-3xl mx-auto py-8'>
-                    <Posts />
-                </div>
-            </div>
-        </main>
-    </>
+  <>
+    <Header />
+    <main>
+      <div className="container mx-auto px-4">
+        <div className='max-w-3xl mx-auto py-8'>
+          <Posts />
+        </div>
+      </div>
+    </main>
+  </>
 );
